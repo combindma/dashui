0d711c0081a508426ba43b41cc0a361c5bfa445f
Omar Herri <omarherri@laptop.local>
2021-09-29 15:59:55 +0100

    wip

--- resources/views/components/confirm-modal.blade.php
+++ resources/views/components/confirm-modal.blade.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                <button type="submit" class="btn w-full sm:w-auto sm:text-sm">
+                <button type="submit" class="btn w-full sm:ml-3 sm:w-auto sm:text-sm">
                     Confirmer
                 </button>
                 <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 sm:mt-0 sm:w-auto sm:text-sm">
